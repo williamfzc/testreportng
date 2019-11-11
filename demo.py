1f674ce789131deddc519e4f6daa81e7c170da98
williamfzc <178894043@qq.com>
2019-11-11 18:39:55 +0800

    chore: add demo

---
+++ demo.py
@@ -0,0 +1,24 @@
+from reportng.case import NGCase
+import unittest
+
+
+class NewCase(NGCase):
+    def test_pass(self):
+        self.assertTrue(True)
+
+    def test_fail(self):
+        self.assertTrue(False)
+
+    def test_error(self):
+        raise RuntimeError
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    suite = unittest.TestSuite(unittest.TestLoader().loadTestsFromTestCase(NewCase))
+    runner.run(suite)
+
+    result = NewCase.ng_result
+    for name, case in result.to_dict().items():
+        print(f"case: {name}, result: {case}")
+        print(type(case.error), case.error, case.traceback)
